f60e7649a112b15e721f1242b2df1a4f6d10c2cb
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-07-07 11:23:08 -0500

    location and hash navigation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
+const api =  axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {
+    'api_key': API_KEY,
+  }
+}); 
+
+
 async function getTrendingMoviesPreview() {
-  const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-  const data = await res.json();
+  const{ data } = await api('trending/movie/day');
 
   const movies = data.results;
   movies.forEach(movie => {
@@ -19,7 +29,31 @@ async function getTrendingMoviesPreview() {
     trendingPreviewMoviesContainer.appendChild(movieContainer);
 
   });
-  console.log({data, movies});
 }
 
-getTrendingMoviesPreview();
+async function getCategoriesPreview() {
+  const {data} = await api('genre/movie/list');
+  const categories = data.genres;
+
+  categories.forEach(category => {
+
+    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list ');
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+    
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.setAttribute('id', 'id' + category.id);
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+
+  });
+  
+}
+
+
+// getCategoriesPreview();
+// getTrendingMoviesPreview();
---
+++ src/navigation.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+
+function navigator() {
+  console.log({ location });
+
+  if (location.hash.startsWith('#trends')) {
+    trendsPage();
+  } else if (location.hash.startsWith('#search=')) {
+    searchPage();
+  }
+  else if (location.hash.startsWith('#movie=')) {
+    moviePage();
+  }
+  else if (location.hash.startsWith('#category=')) {
+    categoriesPage();
+  } else {
+    homePage();
+  }
+  location.hash
+}
+
+function homePage() {
+  console.log('HOME!!!');
+  getTrendingMoviesPreview();
+  getCategoriesPreview();
+}
+
+function categoriesPage() {
+  console.log('CATEGORIES!!!');
+}
+
+function moviePage() {
+  console.log('MOVIE!!!');
+}
+
+function searchPage() {
+  console.log('SEARCH!!!');
+}
+
+function trendsPage() {
+  console.log('TRENDS!!!');
+}
+
+
